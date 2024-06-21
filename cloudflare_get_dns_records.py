825077720a995453c595720157796a4d494aa98e
Selcuk COSAN <selcuk.cosan@gmail.com>
2024-06-21 21:45:16 +0000

    Files

---
+++ cloudflare_get_dns_records.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import pprint
+
+cloudflare_api = "https://api.cloudflare.com/client/v4/"
+zone_id = "4039853908f53d3345344c3d4d634534557f8e2a"
+api_token = "Bearer 234Yht1kn4y234DOh2qoJG32342alx2342lK0C2342kgNL2342"
+headers = {'Authorization': api_token, 'Content-Type':'application/json'}
+
+cloudflare_dns = cloudflare_api + "zones/" + zone_id + "/dns_records"  
+cloudflare_dns_response = requests.get(cloudflare_dns, headers=headers)
+
+if cloudflare_dns_response.status_code == 200:
+    print("Ok")
+else:
+    print(cloudflare_dns_response.status_code)
+
+dns_data = json.loads(cloudflare_dns_response.text)
+pprint.pprint(dns_data)
